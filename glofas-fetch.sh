af09098a876d65e5613ba45fdc82a2dd2b4ccbc0
Eli Tan <5410435+eliedrian@users.noreply.github.com>
2026-06-02 20:57:31 +0800

    fetch: remove testing lines

--- glofas-fetch.sh
+++ glofas-fetch.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 date=$(date '+%Y%m%d')
-date=20260601
 username=$(<$CREDENTIALS_DIRECTORY/username)
 password=$(<$CREDENTIALS_DIRECTORY/password)
-
-exit 0
 
 lftp "$username:$password@aux.ecmwf.int/fc_netcdf/" <<EOF
 set net:max-retries 10
