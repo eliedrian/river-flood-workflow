bfbf4b283271458543038e1f30e68d5ecb8045f3
Eli Tan <5410435+eliedrian@users.noreply.github.com>
2026-07-06 11:12:03 +0800

    fix script crash on no decision

--- river-flood-process.sh
+++ river-flood-process.sh
@@ -18,7 +18,13 @@ fi
 	--date "$date"
 
 DECISION_DIR="$BASE_DIR/data/gold/trigger_decisions/$date"
-DECISION=$(< "$DECISION_DIR/decision.txt")
+DECISION_FILE="$DECISION_DIR/decision.txt"
+if [[ -f "$DECISION_FILE" ]]; then
+	DECISION=$(< "$DECISION_FILE")
+else
+	echo 'No alert requested.'
+	DECISION=""
+fi
 
 if [[ "$DECISION" == "triggered=True" ]]; then
 	echo 'Alert triggered! Sending out alert via email.'
