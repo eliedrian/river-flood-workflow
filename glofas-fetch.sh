d6c59040c525538d7ba85d741c7a8b1d3ef739cb
Eli Tan <5410435+eliedrian@users.noreply.github.com>
2026-07-13 11:35:11 +0800

    download subset of glofas files

--- glofas-fetch.sh
+++ glofas-fetch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+files_to_download=10
 
 if [[ -n "${DEBUG:-}" ]]; then
 	date=$(date -d yesterday '+%Y%m%d')
@@ -11,10 +12,14 @@ echo "$date"
 username=$(<$CREDENTIALS_DIRECTORY/username)
 password=$(<$CREDENTIALS_DIRECTORY/password)
 
-lftp "$username:$password@aux.ecmwf.int/fc_netcdf/" <<EOF
+nums=($(printf '%02d\n' {0..50} | shuf -n "$files_to_download"))
+
+lftp "$username:$password@aux.ecmwf.int/fc_netcdf/$date" <<EOF
 set net:max-retries 10
 set net:reconnect-interval-base 5
 
-mirror --parallel=3 --verbose --continue "$date"
+$(for n in "${nums[@]}"; do
+	printf 'mget --continue "*_%s_*.nc"\n' "$n"
+done)
 bye
 EOF
