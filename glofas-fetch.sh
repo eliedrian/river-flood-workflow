672753b9e415e5d71f8dd936e659805f17c28f41
Eli Tan <5410435+eliedrian@users.noreply.github.com>
2026-07-21 14:45:29 +0800

    attempt fix on fetch

--- glofas-fetch.sh
+++ glofas-fetch.sh
@@ -15,10 +15,13 @@ nums=($(printf '%02d\n' {0..50} | shuf -n "$files_to_download"))
 mkdir "$date"
 
 lftp "$username:$password@aux.ecmwf.int/fc_netcdf/$date" <<EOF
-set net:max-retries 10
+set net:timeout 10
+set net:max-retries 3
 set net:reconnect-interval-base 5
+set net:reconnect-interval-multiplier 1
 
-mget --continue --parallel 3 -O "$date" $(for n in "${nums[@]}"; do
+mget --continue --parallel 3 --verbose --target-directory "$date" \
+$(for n in "${nums[@]}"; do
 	printf '"*_%s_*.nc" ' "$n"
 done)
 bye
