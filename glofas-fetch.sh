13ef48a607f6d2933033f0883a1ac97587dd1db0
Eli Tan <5410435+eliedrian@users.noreply.github.com>
2026-07-21 14:57:33 +0800

    tweak fetch further

--- glofas-fetch.sh
+++ glofas-fetch.sh
@@ -12,15 +12,14 @@ password=$(<$CREDENTIALS_DIRECTORY/password)
 
 nums=($(printf '%02d\n' {0..50} | shuf -n "$files_to_download"))
 
-mkdir "$date"
-
 lftp "$username:$password@aux.ecmwf.int/fc_netcdf/$date" <<EOF
+set cmd:verbose yes
 set net:timeout 10
 set net:max-retries 3
 set net:reconnect-interval-base 5
 set net:reconnect-interval-multiplier 1
 
-mget --continue --parallel 3 --verbose --target-directory "$date" \
+mget -c -P 3 -d -O "$date" \
 $(for n in "${nums[@]}"; do
 	printf '"*_%s_*.nc" ' "$n"
 done)
