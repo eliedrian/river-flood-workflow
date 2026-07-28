33714b6c3d8c3a4fcd0f12bc51e7baca8884ea92
Eli Tan <5410435+eliedrian@users.noreply.github.com>
2026-07-28 21:36:41 +0800

    update fetch for cropped data

--- glofas-fetch.sh
+++ glofas-fetch.sh
@@ -6,28 +6,16 @@ else
 	date=$(date '+%Y%m%d')
 fi
 
-queue_file="$date.manifest"
-
-files_to_download=10
-if [[ ! -s "$queue_file" ]]; then
-	printf '%02d\n' {0..50} | shuf -n "$files_to_download" > "$queue_file"
-fi
-
-mapfile -t nums < "$queue_file"
-
 username=$(<$CREDENTIALS_DIRECTORY/username)
 password=$(<$CREDENTIALS_DIRECTORY/password)
 
-lftp "$username:$password@aux.ecmwf.int/fc_netcdf/$date" <<EOF
+lftp "$username:$password@aux.ecmwf.int/for_Phillippines" <<EOF
 set cmd:verbose yes
 set net:timeout 10
 set net:max-retries 3
 set net:reconnect-interval-base 5
 set net:reconnect-interval-multiplier 1
 
-mget -c -P 3 -d -O "$date" \
-$(for n in "${nums[@]}"; do
-	printf '"*_%s_*.nc" ' "$n"
-done)
+get -c "glofas_areagrid_for_Phillippines_in_Phillippines_$date00.nc" -O "$date"
 bye
 EOF
