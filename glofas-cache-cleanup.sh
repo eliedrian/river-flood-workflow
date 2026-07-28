9c730e4bf0c56fa005a6d1bf22fbd55e2d2c3fd7
Eli Tan <5410435+eliedrian@users.noreply.github.com>
2026-07-28 21:48:11 +0800

    save 30 days worth of glofas data

--- glofas-cache-cleanup.sh
+++ glofas-cache-cleanup.sh
@@ -2,7 +2,7 @@
 
 GLOFAS_CACHE=/var/cache/glofas
 
-find "$GLOFAS_CACHE" -mindepth 1 -maxdepth 1 -type d |
+find "$GLOFAS_CACHE" -mindepth 1 -maxdepth 1 -type f |
 	xargs -r ls -td |
-	tail -n +5 |
+	tail -n +30 |
 	xargs -r rm -rf
