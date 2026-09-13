54691b891c5d69b16d4cb1a84aa9f347be54529a
Eli Tan <5410435+eliedrian@users.noreply.github.com>
2026-09-13 23:35:39 +0800

    fix order of lftp options

--- glofas-fetch.sh
+++ glofas-fetch.sh
@@ -22,6 +22,6 @@ set net:max-retries 3
 set net:reconnect-interval-base 5
 set net:reconnect-interval-multiplier 1
 
-get -c glofas_areagrid_for_Phillippines_in_Phillippines_"$date"00.nc -O "$outdir"
+get -c -O "$outdir" glofas_areagrid_for_Phillippines_in_Phillippines_"$date"00.nc
 bye
 EOF
